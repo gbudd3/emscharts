7642bc2810314756120ac09dd4fd61144273d3b1
George Budd <gbudd@ironkeyboard.com>
2021-12-12 23:03:02 -0500

    update

--- check_responder_names.py
+++ check_responder_names.py
@@ -4,20 +4,29 @@ import csv
 import re
 
 def clean(s):
-    s = re.sub("\(.+\)","",s)
+    s = re.sub("\(.+?\)","",s)
+    s = re.sub("\s*(\w)\.\s*",r"\1",s)
     s = re.sub("-.+$","",s)
+    s = re.sub("\s+"," ",s)
     s = s.strip()
     s = s.lower()
     return s
 
 names = {}
 
-with open("data/Output_of_Response_Time_and_Type.txt") as csvfile:
-    c = csv.reader(csvfile, delimiter='\t')
-    for row in c:
-        for n in row[12].split(","):
-            names[clean(n)]=""
+def main():
+    with open("data/Output_of_Response_Time_and_Type.txt") as csvfile:
+        c = csv.reader(csvfile, delimiter='\t')
+        for row in c:
+            if row[0] == "PRID":
+                continue
+
+            for n in re.split("\s*[,;&]\s*",row[12]):
+                if clean(n):
+                    names[clean(n)]=row[12]
 
 
-for n in sorted(names):
-    print(n)
+    for n in sorted(names):
+        print(n + "\t ===> " + names[n])
+
+main()
